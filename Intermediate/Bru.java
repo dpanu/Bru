3d09cfa5db3aaf9713ec8aa89b5e22610e7799af
dpanu <dpanu@asu.edu>
2016-04-27 00:05:42 -0700

    print and symbol table updated

--- Intermediate/Bru.java
+++ Intermediate/Bru.java
@@ -2,6 +2,8 @@ import java.io.*;
 import java.util.*;
 
 public class Bru {
+	//global symbol table
+	//public static Map<String, String> globalsymtab = new HashMap<String, String>();
 	//current symbol table
 	public static Map<String, String> values = new HashMap<String, String>();
 	//stack for symbol table
@@ -18,187 +20,262 @@ public class Bru {
 		String path = args[0];
 		String line = null;
 		Boolean condition = null;
-		String whilelabel = "";
-		BufferedReader br = new BufferedReader(new FileReader(path));
-		while((line = br.readLine()).equals(".MainMethodStarts") == false);
-		while((line = br.readLine()) != null){
-			String[] command = line.split(" ");
-			switch(command[0]){
-				case "PUSH": 	
-							try{
-								run.push((command[1]));
-							}
-							catch(NumberFormatException e){
-								String val = values.get(command[1]);
-								run.push(val);
-							}
-								
-					     	break;
-				case "LOAD": 	
-							if(values.containsKey(command[1])){
-								run.push(values.get(command[1]));
-							}
-							else{
-								System.out.println("Undeclared variable "+command[1]+", initialize it with some value before passing it to function");
-							}
-							break;
-				case "STORE"://check for type mismatch if variable already exists
-							if ((values.containsKey(command[1])) && (values.get(command[1]).getClass() != run.peek().getClass())){
-								 System.out.println("Type Mismatch error"); 
-								 System.exit(0); //runtime error
-							}
-							else values.put(command[1], (run.pop()));
-				 	      	break;
-				case "ADD": 	run.push(Integer.toString(Integer.parseInt(run.pop()) + Integer.parseInt(run.pop())));
-					    	break;
-				case "MUL": 	run.push(Integer.toString(Integer.parseInt(run.pop()) * Integer.parseInt(run.pop())));
-					    	break;
-				case "SUB": 	run.push(Integer.toString(Integer.parseInt(run.pop()) - Integer.parseInt(run.pop())));
-				     	    	break;
-				case "DIV": 	run.push(Integer.toString(Integer.parseInt(run.pop()) / Integer.parseInt(run.pop())));
-					    	break;
-				case "PRINT": if(command[1].charAt(0) == '\"')
-								System.out.println(line.split(" ", 2)[1].split("\"")[1]);
-							else 
-								System.out.println(values.get(command[1]));
-				  	      	break;
-				case "ME" : int a = Integer.parseInt(run.pop());
-					    	int b = Integer.parseInt(run.pop());
-					    	if(a >= b) condition = true;
-					    	else condition = false;
-					    	break;
-				case "LE" : a = Integer.parseInt(run.pop());
-		    				b = Integer.parseInt(run.pop());
-					    	if(a <= b) condition = true;
-					    	else condition = false;
-						break;
-				case "EQ" : a = Integer.parseInt(run.pop());
-							b = Integer.parseInt(run.pop());
-						if(a == b) condition = true;
-						else condition = false;
-						break;
-				case "NEQ" : 	a = Integer.parseInt(run.pop());
-							b = Integer.parseInt(run.pop());
-						if(a != b) condition = true;
-						else condition = false;
-						break;
-				case "LESS" : 	a = Integer.parseInt(run.pop());
-							b = Integer.parseInt(run.pop());
-						if(a < b) condition = true;
-						else condition = false;
-						break;	
-				case "GRT" : 	a = Integer.parseInt(run.pop());
-							b = Integer.parseInt(run.pop());
-						if(a > b) condition = true;
-						else condition = false;
-						break;
-				case "AND" : 	Boolean abool = Boolean.valueOf(run.pop());
-						Boolean bbool = Boolean.valueOf(run.pop());
-						if (abool == null || bbool == null)System.exit(0); //error to be done
-						if(abool && bbool) condition = true;
-						else condition = false;
-						break;
-				case "OR" : abool = Boolean.valueOf(run.pop());
-						bbool = Boolean.valueOf(run.pop());
-						if (abool == null || bbool == null)System.exit(0); //error to be done
-						if(abool && bbool) condition = true;
-						else condition = false;
-						break;
-				case "IFtrue" : if(!condition){
-							while((line = br.readLine()).equals("Go-Endifelse") == false);  //Semi-Colon used to skip and avoid labeling
-						}	
-						break;
-				case "IFfalse" : if(condition){
-							while((line = br.readLine()).equals("EndIfelse") == false);  //Semi-Colon used to skip labeling and avoid labeling
-						}
-						break;
-				case "Go-Endifelse": break;
-				case "EndIfelse" :break;
-				case "WStart" : whilelabel = command[1];
-						break;
-				case "Whiletrue" : if(!condition){
-							while((line = br.readLine()).equals("WEnd") == false);
-						}
-						break;
-				case "WEnd" :break;	
-				case "Go-WStart": br.close();
-							br = new BufferedReader(new FileReader(path));
-							while((line = br.readLine()).equals("WStart "+whilelabel) == false);
-							break;
-				case "STKDEC": try {StackMap.put(command[1], new Stack<Integer>());}
-							   catch(Exception e) {System.out.println("error in stack declaration" + e);}
-							   finally {break;}
-				case "STORESTK":try {StackMap.get(command[1]).push(Integer.parseInt(run.pop()));}
-								catch(Exception e) {System.out.println("error in stack store" + e);}
-								finally {break;}
-				case "STKPEEK": try {run.push(Integer.toString(StackMap.get(command[1]).peek()));}
-								catch(Exception e) {System.out.println("error in stack peek" + e);}
-								finally {break;}
-				case "STKPOP":  try {int delete = StackMap.get(command[1]).pop();}
-								catch(Exception e) {System.out.println("error in stack pop" + e);}
-								finally {break;}	
-				case "FuncCall":
-								//symtab.push(values);
-								//System.out.println("Calling functinon "+command[1]);
-								funcName = command[1];
-								//System.out.println("Calling functinon "+funcName);
-								while((line = br.readLine()).equals("FuncCall Ends") == false){
-									//System.out.println(line);
-									command = line.split(" ");
-									switch(command[0]){
-										case "PUSH": 	run.push((command[1]));
-											break;
-										case "LOAD": 	
-											if(values.containsKey(command[1])){
-												run.push(values.get(command[1]));
-											}
-											else{
-												System.out.println("Undeclared variable "+command[1]+", initialize it with some value before passing it to function");
-											}
-											break;
+		String whilelabel = null;
+		try{
+			BufferedReader br = new BufferedReader(new FileReader(path));
+			while((line = br.readLine()).equals(".MainMethodStarts") == false);
+			while((line = br.readLine()) != null){
+				String[] command = line.split(" ");
+				switch(command[0]){
+					case "PUSH": 	
+								try{
+									run.push((command[1]));
+								}
+								catch(NumberFormatException e){
+									String val = values.get(command[1]);
+									run.push(val);
+								}
+									
+						     	break;
+					case "LOAD": 	
+								if(values.containsKey(command[1])){
+									run.push(values.get(command[1]));
+								}
+								else{
+									System.out.println("Error:Undeclared variable "+command[1]+", initialize it with some value before using it");
+								}
+								break;
+					case "STORE"://check for type mismatch if variable already exists
+								if ((values.containsKey(command[1])) && (values.get(command[1]).getClass() != run.peek().getClass())){
+									 System.out.println("Error:Type Mismatch"); 
+									 System.exit(0); //runtime error
+								}
+								else values.put(command[1], (run.pop()));
+					 	      	break;
+					case "ADD": 
+								run.push(Integer.toString(Integer.parseInt(run.pop()) + Integer.parseInt(run.pop())));
+						    	break;
+					case "MUL": 
+								run.push(Integer.toString(Integer.parseInt(run.pop()) * Integer.parseInt(run.pop())));
+						    	break;
+					case "SUB": 
+								run.push(Integer.toString(Integer.parseInt(run.pop()) - Integer.parseInt(run.pop())));
+					     	    break;
+					case "DIV": 
+								run.push(Integer.toString(Integer.parseInt(run.pop()) / Integer.parseInt(run.pop())));
+						    	break;
+					case "PRINT": 
+								if(command[1].charAt(0) == '\"')
+									System.out.println(line.split(" ", 2)[1].split("\"")[1]);
+								else if(!(values.containsKey(command[1])))
+									System.out.println("Error:Undeclared variable "+command[1]+", initialize it with some value before using it");
+								else 
+									System.out.println(values.get(command[1]));
+					  	      	break;
+					case "ME" : 
+								int a = Integer.parseInt(run.pop());
+						    	int b = Integer.parseInt(run.pop());
+						    	if(a >= b) 
+						    		condition = true;
+						    	else 
+						    		condition = false;
+						    	break;
+					case "LE" : 
+								a = Integer.parseInt(run.pop());
+			    				b = Integer.parseInt(run.pop());
+						    	if(a <= b)
+						    		condition = true;
+						    	else
+						    		condition = false;
+						    	break;
+					case "EQ" : 
+								a = Integer.parseInt(run.pop());
+								b = Integer.parseInt(run.pop());
+								if(a == b)
+									condition = true;
+								else
+									condition = false;
+								break;
+					case "NEQ" : 	
+								a = Integer.parseInt(run.pop());
+								b = Integer.parseInt(run.pop());
+								if(a != b)
+									condition = true;
+								else
+									condition = false;
+								break;
+					case "LESS" : 	
+								a = Integer.parseInt(run.pop());
+								b = Integer.parseInt(run.pop());
+								if(a < b)
+									condition = true;
+								else
+									condition = false;
+								break;	
+					case "GRT" : 	
+								a = Integer.parseInt(run.pop());
+								b = Integer.parseInt(run.pop());
+								if(a > b) 
+									condition = true;
+								else 
+									condition = false;
+								break;
+					case "AND" : 	
+								Boolean abool = Boolean.valueOf(run.pop());
+								Boolean bbool = Boolean.valueOf(run.pop());
+								if (abool == null || bbool == null){
+									System.out.println("Error:Unassigned boolean variable, assign true or false before use");
+									System.exit(0);
+									}
+								if(abool && bbool) 
+									condition = true;
+								else 
+									condition = false;
+								break;
+					case "OR" : 
+								abool = Boolean.valueOf(run.pop());
+								bbool = Boolean.valueOf(run.pop());
+								if (abool == null || bbool == null){
+									System.out.println("Error:Unassigned boolean variable, assign true or false before use");
+									System.exit(0);
+									}
+								if(abool && bbool)
+									condition = true;
+								else
+									condition = false;
+								break;
+					case "IFtrue" : 
+								if(!condition){
+									while((line = br.readLine()).equals("Go-Endifelse") == false);  
+								}	
+								break;
+					case "IFfalse" : 
+								if(condition){
+									while((line = br.readLine()).equals("EndIfelse") == false); 
+								}
+								break;
+					case "Go-Endifelse":
+										break;
+					case "EndIfelse" :
+										break;
+					case "WStart" : 
+								whilelabel = command[1];
+								break;
+					case "Whiletrue" : 
+									if(!condition){
+										while((line = br.readLine()).equals("WEnd") == false);
+									}
+									break;
+					case "WEnd" :	
+									break;	
+					case "Go-WStart": 
+									br.close();
+									br = new BufferedReader(new FileReader(path));
+									while((line = br.readLine()).equals("WStart "+whilelabel) == false);
+									break;
+					case "STKDEC": 
+								   try {
+									   StackMap.put(command[1], new Stack<Integer>());
+								   }
+								   catch(Exception e) {
+									   System.out.println("Stack declaration error:" + e);
+								   }
+								   finally {
+									   break;
+								   }
+					case "STORESTK":
+									try {
+										StackMap.get(command[1]).push(Integer.parseInt(run.pop()));
+									}
+									catch(Exception e) {
+										System.out.println("Stack error:" + e);
+									}
+									finally {
+										break;
+									}
+					case "STKPEEK": 
+									try {
+										run.push(Integer.toString(StackMap.get(command[1]).peek()));
+									}
+									catch(Exception e) {
+										System.out.println("Stack error:" + e);
+									}
+									finally {
+										break;
+									}
+					case "STKPOP":  
+									try {
+										int delete = StackMap.get(command[1]).pop();
+									}
+									catch(Exception e) {
+										System.out.println("Stack error:" + e);
+									}
+									finally {
+										break;
+									}	
+					case "FuncCall":
+									funcName = command[1];
+									while((line = br.readLine()).equals("FuncCall Ends") == false){
+										command = line.split(" ");
+										switch(command[0]){
+											case "PUSH": 	run.push((command[1]));
+												break;
+											case "LOAD": 	
+												if(values.containsKey(command[1])){
+													run.push(values.get(command[1]));
+												}
+												else{
+													System.out.println("Error: Undeclared variable "+command[1]+", initialize it with some value before using.");
+												}
+												break;
+											default: ;
+										  } // End of inner switch series inside the while loop
+									} //End of while
+									//push current function symbol table to stack
+									symtab.push(new HashMap<String,String>(values));
+									//push file reader to stack
+									fileReader.push(new BufferedReader(br));
+									//define new symbol table
+									values = new HashMap<String,String>();
+									//start searching for function
+									br = new BufferedReader(new FileReader(path));	
+									while((line = br.readLine()).equals("FuncDef "+funcName) == false);
+									while((line = br.readLine()).equals(".funcBodyStarts "+funcName) == false){
+										command = line.split(" ");
+										switch(command[0]){
+										case "STORE"://check for type mismatch if variable already exists
+												if ((values.containsKey(command[1])) && (values.get(command[1]).getClass() != run.peek().getClass())){
+													System.out.println("Error:Type Mismatch"); 
+													System.exit(0); //runtime error
+												}
+												else {
+													values.put(command[1], (run.pop()));
+												}
+												break;
 										default: 	;//System.out.println("Not able to identify the argument passed to function " + command[0]);
 									  } // End of inner switch series inside the while loop
-								} //End of while
-								symtab.push(new HashMap<String,String>(values));//push current function symbol table to stack
-								fileReader.push(new BufferedReader(br));
-								//break;
-								
-				case "FuncDef":
-							   //if(!command[1].equals("ends")){
-								br = new BufferedReader(new FileReader(path));	
-								while((line = br.readLine()).equals("FuncDef "+funcName) == false); //Semi-Colon used to skip labeling, shortcut method
-								//System.out.println("Found "+line);
-								while((line = br.readLine()).equals(".funcBodyStarts "+funcName) == false){
-									command = line.split(" ");
-									switch(command[0]){
-									case "STORE"://check for type mismatch if variable already exists
-											if ((values.containsKey(command[1])) && (values.get(command[1]).getClass() != run.peek().getClass())){
-												System.out.println("Type Mismatch error"); 
-												System.exit(0); //runtime error
-											}
-											else {
-												//System.out.println(values.get(command[1])); 
-												values.put(command[1], (run.pop()));
-											}
+									}//end of while
+									break;
+					case "RETURN" : ;
+					case ".funcbodyends" : //return to previous functions' symbol table, current function scope ends
+										   values = symtab.pop();
+										   //return to instruction pointer
+										   br = fileReader.pop();
+					case ".funcends": 
 											break;
-									default: 	;//System.out.println("Not able to identify the argument passed to function " + command[0]);
-								  } // End of inner switch series inside the while loop
-								}//end of while
-								break;
-				case "RETURN" : ;
-				case ".funcbodyends" : values = symtab.pop();
-									   br = fileReader.pop();
-									   //System.out.println(mp.get(run.pop()));break;
-				case ".funcends"     : break;
-				case ".MainMethodEnds":break;			
-				case "" : 	break;
-				default: 	System.out.println("Error: command not found " + command[0]); //System.exit(0);
-			}	
+					case ".MainMethodEnds":
+											break;			
+					case "" : 	
+											break;
+					default: 
+									System.out.println("Error: runtime command not found " + command[0]); //System.exit(0);
+				}	
+			}
+			br.close();	
+		}//end of try
+		catch(Exception e){
+			System.out.println("Error:" + e);
 		}
-		br.close();		
-}
-}
-
-
-
-
+			
+}//end of main
+}//end of class
